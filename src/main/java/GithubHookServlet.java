b12affc93c5ea8fffe55cbb2b78d3bdc8bdb6c4b
bacquet <nathan.bacquet@etudiant.univ-lille1.fr>
2016-10-06 16:41:07 +0200

    Added :    - download archive from a push (via webhook)    -unzip it

--- src/main/java/GithubHookServlet.java
+++ src/main/java/GithubHookServlet.java
@@ -1,9 +1,15 @@
+import org.apache.sling.commons.json.JSONException;
+import org.apache.sling.commons.json.JSONObject;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Enumeration;
+import java.util.Map;
 
 /**
  * Created by bacquet on 03/10/16.
@@ -19,12 +25,28 @@ public class GithubHookServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        File log = new File("testReq");
-        FileWriter fileWriter = new FileWriter(log);
-        Enumeration<String> headerNames = req.getHeaderNames();
-        while (!headerNames.hasMoreElements()){
-            fileWriter.write(headerNames.nextElement());
+        System.out.println("coucou post");
+        StringBuilder builder = new StringBuilder();
+        String aux = "";
+
+        while ((aux = req.getReader().readLine()) != null) {
+            builder.append(aux);
         }
-        fileWriter.close();
+
+        String text = builder.toString();
+        try {
+            JSONObject json = new JSONObject(text);
+
+            String archive_url = json.getJSONObject("repository").getString("archive_url");
+            archive_url=archive_url.replace("{archive_format}{/ref}","");
+            String ref = json.getString("ref");
+            System.out.println(ref);
+            URL url = new URL(archive_url + "zipball/" + ref);
+            System.out.println(url);
+            File archive = HttpDownloadUtility.downloadFile(url.toString(),"/home/m2iagl/bacquet/Documents/tmp");
+            UnzipUtility.unzip(archive.getAbsolutePath(),"/home/m2iagl/bacquet/Documents/tmp");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
